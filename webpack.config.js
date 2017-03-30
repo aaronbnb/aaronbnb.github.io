643c509266b05e7d31dfc4277743a49a07b92fad
Aaron Farber <aaronfarber@Aarons-MacBook-Pro.local>
2017-03-30 11:08:05 -0700

    input field

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,11 @@
 var path = require('path');
-
+const ASSET_PATH = 'https://aaronbnb.github.io';
 module.exports = {
   entry: './lib/main.jsx',
   output: {
     path: path.resolve(__dirname),
     filename: 'bundle.js',
+    publicPath: ASSET_PATH
   },
   devtool: 'source-map',
   resolve: {
